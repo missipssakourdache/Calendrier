2f6a865d6d7969bad59b1e3fbd746c95270aeee3
missipssakourdache <m.kourdache@yahoo.fr>
2018-04-10 09:51:12 +0200

    10/04/18

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Http } from '@angular/http';
 
+const styles   = require('./login.component.css');
+const template = require('./login.component.html');
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public router: Router, public http: Http) {
   }
 
+  login(event, username, password) {
+    event.preventDefault();
+    let body = JSON.stringify({ username, password });
+    this.http.post('http://localhost:3001/sessions/create', body, { headers: contentHeaders })
+      .subscribe(
+        response => {
+          localStorage.setItem('id_token', response.json().id_token);
+          this.router.navigate(['home']);
+        },
+        error => {
+          alert(error.text());
+          console.log(error.text());
+        }
+      );
+  }
+
+  signup(event) {
+    event.preventDefault();
+    this.router.navigate(['signup']);
+  }
+  
 }
